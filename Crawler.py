3234cf848fb7d312541614f964d7110bd54f9c35
mark19891107 <mark19891107@gmail.com>
2017-02-08 10:32:45 +0800

    add save by date

--- Crawler.py
+++ Crawler.py
@@ -20,9 +20,22 @@ def getDataTable():
     table_data = [[cell.text for cell in row("td")] for row in soup.find_all('table')[2]("tr")]
     return table_data
 
-def saveData(dataRow):
+def saveDataByFood():
+    for row in DATATABLE:
+        # 略過每個市場標頭
+        if u"跳到" in row[0] or len(row) == 0:
+            continue
+        # 抓出大種類(但目前沒有用到)
+        elif len(row) == 1:
+            foodType = row[0].strip()
+            print foodType
+        # 真正的資料內容
+        else:
+            saveDataByFoodRow(row)
+
+def saveDataByFoodRow(dataRow):
     food = dataRow[0]
-    filename = FOLDER + "/" + food + ".json"
+    filename = FOLDER + "/byFood/" + food + ".json"
     dataArray = []
 
     if os.path.exists(filename):
@@ -42,20 +55,37 @@ def saveData(dataRow):
             dataArray.append(record)
         jsonFile.write(json.dumps(dataArray, ensure_ascii=False).encode('utf8'))
 
+def saveDataByDate():
+    filename = FOLDER + "/byDate/" + DATE + ".csv"
+    with open(filename, "w") as csvFile:
+        
+        csvFile.write("食物,"+",".join(DATATABLE[0][1:]).encode('utf8')+"\n")
+
+        for row in DATATABLE:
+            # 略過每個市場標頭
+            if u"跳到" in row[0] or len(row) == 0:
+                continue
+            # 抓出大種類(但目前沒有用到)
+            elif len(row) == 1:
+                foodType = row[0].strip()
+                print foodType
+            # 真正的資料內容
+            else:
+                csvFile.write(",".join(row).encode('utf8')+"\n")
+
+
 MARKETS = []
 FOODTYPE = ""
 DATE = sys.argv[1]
-FOLDER = "vegeprice"
+FOLDER = "priceArchive"
 DATATABLE = getDataTable()
 MARKETS = DATATABLE[0]
 
-for row in DATATABLE:
-    # 略過每個市場標頭
-    if u"跳到" in row[0] or len(row) == 0:
-        continue
-    # 抓出大種類
-    elif len(row) == 1:
-        foodType = row[0].strip()
-    # 真正的資料內容
-    else:
-        saveData(row)
+if not os.path.exists(FOLDER):
+    os.makedirs(FOLDER)
+
+if not os.path.exists(FOLDER + "/byDate/"):
+    os.makedirs(FOLDER + "/byDate/")
+
+# saveDataByFood()
+saveDataByDate()
