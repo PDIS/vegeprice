5795b88568f7dcef21e83f09871e41177a7a3d28
mark19891107 <mark19891107@gmail.com>
2017-02-08 13:57:24 +0800

    修改byFood的資料儲存格式

--- Crawler.py
+++ Crawler.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 #coding: utf8
 import httplib
-import datetime
 import urllib
-import json
 import os.path
 import sys
 from bs4 import BeautifulSoup
+
+def checkFolder():
+    if not os.path.exists(FOLDER):
+        os.makedirs(FOLDER)
+    if not os.path.exists(FOLDER + "/byDate/"):
+        os.makedirs(FOLDER + "/byDate/")
+    if not os.path.exists(FOLDER + "/byFood/"):
+        os.makedirs(FOLDER + "/byFood/")
 
 def getDataTable():
     params = urllib.urlencode({'yy': int(DATE.split("-")[0])-1911, 'mm': DATE.split("-")[1], 'dd': DATE.split("-")[2]})
@@ -27,65 +33,48 @@ def saveDataByFood():
             continue
         # 抓出大種類(但目前沒有用到)
         elif len(row) == 1:
-            foodType = row[0].strip()
-            print foodType
+            # foodType = row[0].strip()
+            continue
         # 真正的資料內容
         else:
             saveDataByFoodRow(row)
 
 def saveDataByFoodRow(dataRow):
     food = dataRow[0]
-    filename = FOLDER + "/byFood/" + food + ".json"
-    dataArray = []
+    filename = FOLDER + "/byFood/" + food + ".csv"
+    if not os.path.exists(filename):
+        with open(filename, "a") as csvFile:
+            csvFile.write("日期,市場,價格\n")
 
-    if os.path.exists(filename):
-        with open(filename, "r") as jsonFile:
-            try: 
-                dataArray = json.load(jsonFile)
-            except ValueError: 
-                dataArray = []
-        
-    with open(filename, "w+") as jsonFile:
+    dataArray = []
+    with open(filename, "a") as csvFile:
         for idx, price in enumerate(dataRow[1:]):
-            record = {}
-            record['date'] = DATE
-            record['market'] = MARKETS[idx+1]
-            record['price'] = price
-            record['timestamp'] = int(datetime.datetime.strptime(DATE, '%Y-%m-%d').strftime('%s'))
-            dataArray.append(record)
-        jsonFile.write(json.dumps(dataArray, ensure_ascii=False).encode('utf8'))
+            dataArray.append(("%s,%s,%s" % (DATE, MARKETS[idx], price)).encode('utf8'))
+        csvFile.write("\n".join(dataArray)+"\n")
 
 def saveDataByDate():
     filename = FOLDER + "/byDate/" + DATE + ".csv"
     with open(filename, "w") as csvFile:
-        
-        csvFile.write("食物,"+",".join(DATATABLE[0][1:]).encode('utf8')+"\n")
-
+        csvFile.write("食物,"+",".join(MARKETS).encode('utf8')+"\n")
         for row in DATATABLE:
             # 略過每個市場標頭
             if u"跳到" in row[0] or len(row) == 0:
                 continue
             # 抓出大種類(但目前沒有用到)
             elif len(row) == 1:
-                foodType = row[0].strip()
-                print foodType
+                # foodType = row[0].strip()
+                continue
             # 真正的資料內容
             else:
-                csvFile.write(",".join([price.replace(",","") for price in row]).encode('utf8')+"\n")
+                csvFile.write(",".join([price.replace(",", "") for price in row]).encode('utf8')+"\n")
 
 
-MARKETS = []
 FOODTYPE = ""
 DATE = sys.argv[1]
 FOLDER = "priceArchive"
 DATATABLE = getDataTable()
-MARKETS = DATATABLE[0]
+MARKETS = DATATABLE[0][1:]
 
-if not os.path.exists(FOLDER):
-    os.makedirs(FOLDER)
-
-if not os.path.exists(FOLDER + "/byDate/"):
-    os.makedirs(FOLDER + "/byDate/")
-
-# saveDataByFood()
-saveDataByDate()
+checkFolder()
+saveDataByFood()
+# saveDataByDate()
