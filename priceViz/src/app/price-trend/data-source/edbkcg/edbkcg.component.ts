c68e1fee671e99aa5c151e3054f5261e5901a43b
mark19891107 <mark19891107@gmail.com>
2017-02-21 17:47:55 +0800

    add mean and median

--- priceViz/src/app/price-trend/data-source/edbkcg/edbkcg.component.ts
+++ priceViz/src/app/price-trend/data-source/edbkcg/edbkcg.component.ts
@@ -1,4 +1,4 @@
-import {ChartDataService} from '../../chart/chart-data.service';
+import { ChartDataService } from '../../chart/chart-data.service';
 import { Chart } from './../../chart/chart';
 import { EDBKCGConfig } from './edbkcg.config';
 import { Http } from '@angular/http/src/http';
@@ -18,7 +18,7 @@ export class EdbkcgComponent implements OnInit {
   markets: Array<string> = []
   allData: Array<Chart> = []
 
-  constructor(private http: Http,private chartDataService:ChartDataService) { }
+  constructor(private http: Http, private chartDataService: ChartDataService) { }
 
   ngOnInit() {
     this.http.get(EDBKCGConfig.FoodURL)
@@ -30,14 +30,14 @@ export class EdbkcgComponent implements OnInit {
     this.http.get(EDBKCGConfig.MarketURL)
       .map(data => data.json())
       .subscribe(data => {
-        // this.markets = ["平均價格"].concat(data);
-        this.markets = data;
+        this.markets = ["平均價格", "中位價格"].concat(data);
+        // this.markets = data;
       })
   }
 
   private addItem() {
-    // this.selectedSets.push(new EDBKCGData("","平均價格"));
-    this.selectedSets.push(new EDBKCGData("",""));
+    this.selectedSets.push(new EDBKCGData("", "平均價格"));
+    // this.selectedSets.push(new EDBKCGData("", ""));
   }
 
   private deleteItem(index) {
@@ -46,33 +46,72 @@ export class EdbkcgComponent implements OnInit {
   }
 
   private onSelectChange(index) {
-    
+
     let set = this.selectedSets[index];
-    
-    if(set.food==="" || set.market==="")
+
+    if (set.food === "" || set.market === "")
       return
 
-    this.http.get( EDBKCGConfig.byFoodURL + set.food + '.csv')
-    .map(data=>{
-      var chart:Chart = new Chart()
-      chart.y_axis = 0
-      chart.data = this.csvToChartData(data['_body'], set.market)
-      chart.label = set.food + "-" + set.market;
-      return chart;
-    })
-    .subscribe(chart=>{
-      this.allData[index]=chart;
-      this.chartDataService.addChartDataSet("edbkcg",this.allData);
-    })
+    this.http.get(EDBKCGConfig.byFoodURL + set.food + '.csv')
+      .map(data => {
+        var chart: Chart = new Chart()
+        chart.y_axis = 0
+        chart.data = this.csvToChartData(data['_body'], set.market)
+        chart.label = set.food + "-" + set.market;
+        return chart;
+      })
+      .subscribe(chart => {
+        this.allData[index] = chart;
+        this.chartDataService.addChartDataSet("edbkcg", this.allData);
+      })
   }
 
-  private csvToChartData(csv:string, marketFilter:string){
+  private csvToChartData(csv: string, marketFilter: string) {
     let chartData = {};
-    csv.split("\n").splice(1).forEach((row)=>{
-        let [date,market,price] = row.split(",",3)
-        chartData[date] = price
+    let tmpData = []
+    csv.split("\n").splice(1).forEach((row) => {
+      let [date, market, price] = row.split(",", 3)
+      tmpData.push({ date: date, market: market, price: price })
     });
-    
+
+    if (marketFilter === "平均價格") {
+      // group by same date
+      let groupByDate = _.groupBy(tmpData, function (a) { return a['date'] })
+      // foreach date compute avg
+      Object.keys(groupByDate).forEach((date) => {
+        // fetch field price, cast to int and filter NaN 
+        let priceList = _.chain(groupByDate[date])
+          .map((record) => parseInt(record['price']))
+          .filter(price => price > 0)
+          .value()
+        chartData[date] = _.mean(priceList);
+      })
+    }
+    else if (marketFilter === "中位價格") {
+      // group by same date
+      let groupByDate = _.groupBy(tmpData, function (a) { return a['date'] })
+      // foreach date compute avg
+      Object.keys(groupByDate).forEach((date) => {
+        // fetch field price, cast to int and filter NaN 
+        let priceList = _.chain(groupByDate[date])
+          .map((record) => parseInt(record['price']))
+          .filter(price => price > 0)
+          .value()
+        if (priceList.length !== 0) {
+          chartData[date] = priceList[Math.ceil(priceList.length/2)]
+        }
+      })
+    }
+    else {
+      tmpData.forEach(data => {
+        if (data['market'] === marketFilter) {
+          chartData[data['date']] = parseInt(data['price'])
+        }
+      })
+    }
+
+    console.log(chartData)
+
     return chartData;
   }
 
